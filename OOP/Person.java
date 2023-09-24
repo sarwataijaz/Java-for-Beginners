8d66c15a71c5dc2e4415717aafa6793f0eae5451
sarwataijaz <sarwataijaaz@gmail.com>
2023-09-24 23:23:02 +0500

    Single and Multilevel Inheritances

--- OOP/Person.java
+++ OOP/Person.java
@@ -1,4 +1,4 @@
-// concept of single inheritance in java
+// concept of single and multilevel inheritance in java
 public class Person {
 
     private int age;
@@ -24,22 +24,44 @@ public class Person {
 class Student extends Person {
 
     private String rollnum;
-    private int batch;
+    private int year;
 
     void setRollnum(String r) {
         rollnum = r;
     }
 
-    void setBatch(int b) {
-        batch = b;
+    void setYear(int b) {
+        year = b;
     }
 
     String getRollnum() {
         return rollnum;
     }
 
-    int getBatch() {
-        return batch;
+    int getYear() {
+        return year;
+    }
+}
+
+class UniversityStudent extends Student {
+
+    private int semester;
+    private String uniName;
+
+    void setUniName (String s) {
+        uniName = s;
+    }
+
+    void setSemester(int n) {
+        semester = n;
+    }
+
+    int getSemester() {
+        return semester;
+    }
+
+    String getUniName() {
+        return uniName;
     }
 }
 
@@ -47,13 +69,18 @@ class Example {
 
     public static void main(String[] args) {
 
-        Student s1 = new Student();
+        UniversityStudent s1 = new UniversityStudent();
         s1.setName("Sarwat");
         s1.setRollnum("22SW024");
-        s1.setBatch(22);
+        s1.setYear(22);
+        s1.setUniName("MUET");
+        s1.setSemester(2);
+
 
         System.out.println("Name: " + s1.getName());
+        System.out.println("University name: " + s1.getUniName());
         System.out.println("Roll number: " + s1.getRollnum());
-        System.out.println("Batch: " + s1.getBatch());
+        System.out.println("Semester: " + s1.getSemester());
+        System.out.println("Year: " + s1.getYear());
     }
 }
