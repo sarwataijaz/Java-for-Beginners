dd2a2fbd0de05aabaa30651be68ebd15ea31c210
sarwataijaz <sarwataijaaz@gmail.com>
2023-10-05 19:24:06 +0500

    Constructors Chaining

--- OOP/Constructors.java
+++ OOP/Constructors.java
@@ -9,13 +9,19 @@ class A {
 class B extends A {
 
     public B() {
-        // by default super() for calling the A class constructor first
+        this(4); // now super wont be given by compiler as only one of the keywords i.e this/super can exist at a time
         System.out.println("B class constructor 1.");
+    }
+
+
+    public B(int k) {
+        // this will first call the constructor of parent class by default super keyword
+        System.out.println("B class constructor 2.");
     }
 }
 public class Constructors {
 
     public static void main(String[] args) {
-        new B();
+        new B(); // constructor without parameter (default constructor) will be called
     }
 }
