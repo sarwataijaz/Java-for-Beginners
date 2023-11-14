4a59592e5a39dc97ba83648a36a6894949ddcff6
sarwataijaz <sarwataijaaz@gmail.com>
2023-11-14 16:18:26 +0500

    Switiching between two images in a single scene

---
+++ ImageViews/src/main/java/com/example/switchscenes/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.switchscenes;
+
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Arc;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloController {
+
+    @FXML
+    ImageView image2;
+    @FXML
+    ImageView image1;
+    @FXML
+
+  public void switchToImage2() {
+
+
+        image2.setLayoutX(231);
+        image2.setLayoutY(54);
+
+        image1.setVisible(false);
+        image2.setVisible(true);
+
+        ImageView temp = image2;
+        image2 = image1;
+        image1 = temp;
+    }
+
+
+}
